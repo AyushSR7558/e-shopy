085f9c8b83baa7a593b17a4decc995b3f86f3c4b
Ayush <ayushraut7558@gmail.com>
2026-01-25 20:15:17 +0530

    Add/ Header

---
+++ frontend/src/configs/constants.tsx
@@ -0,0 +1,21 @@
+import { title } from "process";
+
+export const navItems: NavItemsTypes[] = [
+    {
+        title: "Home",
+        href: "/"
+    },
+    {
+        title: "Products",
+        href: "/products"
+    },
+    {
+        title: "Shops",
+        href: "/shops"
+    },
+    {
+        title: "Offer",
+        href: "/offer"
+    }
+
+]
